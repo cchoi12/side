847ee8c60e868ad7a3b7ecf315fb6b2ef14610c7
Christopher Choi <cdddchris@gmail.com>
2018-01-16 01:55:05 -0800

    add in new helpers, get deleted to work..but how

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :set_movie, only: %i[index new create]
+  before_action :set_movie, only: %i[index new create destroy edit]
 
   def index
     @reviews = @movie.reviews.order(created_at: :desc)
@@ -17,6 +17,17 @@ class ReviewsController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def destroy
+    @review = @movie.reviews.find(params[:id])
+
+    @review.destroy
+    redirect_to movie_reviews_path(@movie), notice: "Review successfully deleted!"
+  end
+
+  def edit
+    @review = @movie.reviews
   end
 
   private
